0362abd6a9f74352c41b1ea57b102f1be3d4ae98
Ömür <omur.turan@pisano.co>
2016-08-31 17:17:29 +0300

    Add a method to return moji

--- lib/gemojione.rb
+++ lib/gemojione.rb
@@ -112,6 +112,23 @@ module Gemojione
     safe_string
   end
 
+  def self.replace_named_moji_with_mojis(string)
+    return string unless string
+    unless string.match(index.shortname_moji_regex)
+      return safe_string(string)
+    end
+
+    safe_string = safe_string(string.dup)
+    safe_string.gsub!(index.shortname_moji_regex) do |code|
+      name = code.tr(':','')
+      moji = index.find_by_name(name)['moji']
+      moji
+    end
+    safe_string = safe_string.html_safe if safe_string.respond_to?(:html_safe)
+
+    safe_string
+  end
+
   def self.replace_ascii_moji_with_images(string)
     return string unless string
     unless string.match(index.ascii_moji_regex)
