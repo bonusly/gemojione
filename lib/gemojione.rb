62a15138108d9e1387eb946fcf2f18bbffabd790
L. E. Segovia <13498015+amyspark@users.noreply.github.com>
2019-09-23 12:45:20 +0000

    Add a way to cache and return SVG files

--- lib/gemojione.rb
+++ lib/gemojione.rb
@@ -23,6 +23,8 @@ module Gemojione
 
   @escaper = defined?(EscapeUtils) ? EscapeUtils : CGI
 
+  @emoji_svg = {}
+
   def self.asset_host
     @asset_host || 'http://localhost:3000'
   end
@@ -198,5 +200,12 @@ module Gemojione
 
   def self.sprites_path
     File.expand_path("../assets/sprites", File.dirname(__FILE__))
+  end
+
+  def self.svg_for_name(name) # heart, not :heart:
+    if index.find_by_name(name)
+      moji = index.find_by_name(name)
+      @emoji_svg[name] ||= File.read(File.absolute_path(File.expand_path("../assets/svg", File.dirname(__FILE__)) + "/#{moji['unicode']}.svg"))
+    end
   end
 end
